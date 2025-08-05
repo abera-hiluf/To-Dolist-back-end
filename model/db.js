f0d90b4208915a0cdb8205171e220e40bc221eae
abz <teshalea000@gmail.com>
2025-08-05 10:38:52 +0530

    change the database from local to Tidb

--- model/db.js
+++ model/db.js
@@ -1,9 +1,29 @@
-const mysql2 = require('mysql2');
+// const mysql2 = require('mysql2');
+// require("dotenv").config();
+// const connection = mysql2.createPool({
+//   host: process.env.DB_HOST ,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+// module.exports = connection.promise();
+
+
+const mysql2 = require("mysql2");
 require("dotenv").config();
+// const fs = require("fs"); 
+
 const connection = mysql2.createPool({
-  host: process.env.DB_HOST ,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // Add this line for TiDB port
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true, // Quick method without CA cert
+    // For secure method with CA cert:
+    // ca: fs.readFileSync('./certs/ca.pem')
+  },
 });
+
 module.exports = connection.promise();
